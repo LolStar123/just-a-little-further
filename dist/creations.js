b6c69a704104bad0a76421e143edc1a41f2acc8d
Atul Kanodia <20664456+LolStar123@users.noreply.github.com>
2026-09-24 20:23:49 +0100

    Personalise Smoothtato description

--- dist/creations.js
+++ dist/creations.js
@@ -127,7 +127,7 @@ export const projects={
     "title": "smoothtato",
     "short": "Smoothtato",
     "category": "PATH OF EXILE / PERFORMANCE",
-    "caption": "changes graphics to boost performance and customise visuals in path of exile.",
+    "caption": "my pc sucked so i made a graphics changer app to boost performance and customise visuals in path of exile.",
     "description": "Choose which graphics to remove, match cosmetic effects to your skills, and save the combined setup as a reusable preset. Restore the original files when you want to switch back.",
     "scene": "smoothtato",
     "note": "",
